5d7cc82034ef02e6e438a4caccc4ca6d60941fcd
Zeikar <zeikar@naver.com>
2021-02-17 23:38:12 +0900

    [#20] Add chap19 codes

--- 19-Webcam-Fun/scripts.js
+++ 19-Webcam-Fun/scripts.js
@@ -3,3 +3,99 @@ const canvas = document.querySelector(".photo");
 const ctx = canvas.getContext("2d");
 const strip = document.querySelector(".strip");
 const snap = document.querySelector(".snap");
+
+function getVideo() {
+  navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then((localMediaStream) => {
+      console.log(localMediaStream);
+      video.srcObject = localMediaStream;
+      video.play();
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+
+function paintToCanvas(width, height) {
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+
+    // take the pixels out
+    let pixels = ctx.getImageData(0, 0, width, height);
+
+    // TODO 여러 캔버스에 출력
+    pixels = redEffect(pixels);
+    ctx.putImageData(pixels, 0, 0);
+  }, 16);
+}
+
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i] = pixels.data[i] + 100; // r
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; // g
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // b
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 150] = pixels.data[i]; // r
+    pixels.data[i + 100] = pixels.data[i + 1]; // g
+    pixels.data[i - 50] = pixels.data[i + 2]; // b
+  }
+  return pixels;
+}
+
+function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll(".rgb input").forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    const red = pixels.data[i];
+    const green = pixels.data[i + 1];
+    const blue = pixels.data[i + 2];
+
+    if (
+      red >= levels.rmin &&
+      red <= levels.rmax &&
+      green >= levels.gmin &&
+      green <= levels.gmax &&
+      blue >= levels.bmin &&
+      blue <= levels.bmax
+    ) {
+      pixels.data[i + 3] = 0;
+    }
+  }
+  return pixels;
+}
+
+video.addEventListener(
+  "loadedmetadata",
+  function (e) {
+    paintToCanvas(this.videoWidth, this.videoHeight);
+  },
+  false
+);
+
+function takePhoto() {
+  snap.currentTime = 0;
+  snap.play();
+
+  const data = canvas.toDataURL("image/jpeg");
+  const link = document.createElement("a");
+  link.href = data;
+  link.setAttribute("download", "handsome");
+  link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
+
+  strip.insertBefore(link, strip.firstChild);
+}
+
+getVideo();
